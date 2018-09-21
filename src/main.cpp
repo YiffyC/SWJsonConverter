2062c9f0409e3137cd5afb7912f73eb2b43e5224
Bérenger Thévenet <berenger.thevenet@laposte.net>
2018-09-21 23:48:13 +0200

    toujours pas - error 11

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
-#include "../include/nlohmann/json_fwd.hpp" //json parser
+#include <iomanip>
 #include "nlohmann/json.hpp"
 #include <fstream>
+
 using json = nlohmann::json;
 
 int main()
 {
 
+    //read from file
+    std::ifstream ifs("test.json");
 
-    std::ifstream i("../files/json/mylist.json");
-    json jMyList = json::parse(i);
+    //create object
+    json j(ifs);
+
+    //At this poinr "process finish with error 11
+
+    //dump object as a string
+    //std::string s = j.dump();
+
+    //display
+    //std::cout << j.dump(4) << std::endl;
 
 
-    std::cout << "j" << std::endl;
     return 0;
-
 
 
 }
