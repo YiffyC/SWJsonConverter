ed6c11132cf105d7c5d4945278552822f9cfe3bc
Bérenger Thévenet <berenger.thevenet@laposte.net>
2018-09-28 08:05:25 +0200

    new lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <iomanip>
-#include "nlohmann/json.hpp"
+//#include "nlohmann/json.hpp"
+#include "include/JsonBox-master/include/JsonBox.h"
+#include "include/JsonBox-master/include/JsonBox/Value.h"
 #include <fstream>
 
-using json = nlohmann::json;
 
 int main()
 {
@@ -11,13 +12,8 @@ int main()
     //read from file
     std::ifstream ifs("test.json");
 
-    //create object
-    json j(ifs);
-
-    //At this poinr "process finish with error 11
-
-    //dump object as a string
-    //std::string s = j.dump();
+    JsonBox::Value v2;
+    v2.loadFromFile("test.json");
 
     //display
     //std::cout << j.dump(4) << std::endl;
